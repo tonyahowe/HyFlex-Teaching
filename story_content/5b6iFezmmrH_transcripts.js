30b7c74510dbf42a6d595c3010406870ae58ddee
Tonya Howe <tml.howe@gmail.com>
2024-08-08 13:06:57 -0400

    First push
    Two large video files could not be pushed.

---
+++ story_content/5b6iFezmmrH_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":13240,"text":"To simplify the process, we\r\nsuggest simply turning the room"},{"start":18326,"text":"camera around so that it doesn't\r\nface you, but instead faces the"},{"start":23836,"text":"students in the classroom."},{"start":26800,"text":"You can do this by simply\r\npicking up the camera and"},{"start":30003,"text":"turning it around as you see\r\nhere."},{"start":33480,"text":"Don't forget to return the\r\ncamera to its original state"},{"start":36571,"text":"when you leave."}]}]};
+    window.globalLoadJsAsset('story_content/5b6iFezmmrH_transcripts.js', JSON.stringify(data));
+})();
---
+++ story_content/6Her0Ed5BIb_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":7320,"text":"It may seem kind of obvious, but\r\ndo keep in mind that the login"},{"start":13104,"text":"process in your classroom will\r\nlikely take anywhere from 1 1/2"},{"start":18798,"text":"minutes to 3 minutes, and this\r\nis time that you need to build"},{"start":24402,"text":"into your schedule."}]}]};
+    window.globalLoadJsAsset('story_content/6Her0Ed5BIb_transcripts.js', JSON.stringify(data));
+})();
